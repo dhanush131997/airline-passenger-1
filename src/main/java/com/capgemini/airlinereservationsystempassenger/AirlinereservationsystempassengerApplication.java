0795c08ae48518b32c170f5c41bd51c7fdd14c50
dhanush131997 <dhanushvenkatesh176@gmail.com>
2020-04-09 11:13:15 +0530

    airline-passenger(9th april)

---
+++ src/main/java/com/capgemini/airlinereservationsystempassenger/AirlinereservationsystempassengerApplication.java
@@ -0,0 +1,13 @@
+package com.capgemini.airlinereservationsystempassenger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirlinereservationsystempassengerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirlinereservationsystempassengerApplication.class, args);
+	}
+
+}
